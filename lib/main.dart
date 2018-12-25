5c9127489004e4c3249b7eaffaaf5cb60db2d753
kishan2612 <36294687+kishan2612@users.noreply.github.com>
2018-12-25 18:33:29 +0530

    Animated leading icon is included

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,53 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 100), value: 1.0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  bool _isBackpanelVisible(){
+    final AnimationStatus status = _controller.status;
+
+    return status == AnimationStatus.completed || status == AnimationStatus.forward;
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-       
         title: Text("Backdrop"),
+        leading: IconButton(
+          onPressed: () {
+            _controller.fling(velocity: _isBackpanelVisible()? -1.0:1.0);
+          }, 
+          icon: AnimatedIcon(
+            icon: AnimatedIcons.close_menu,
+            progress: _controller.view,
+          ),
+        ),
       ),
-      body: Container(
-        child: Text("Hello world"),
+      body: Center(
+        child: Container(
+          child: Text("Hello world"),
+        ),
       ),
     );
   }
